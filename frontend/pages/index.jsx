d33cfade83a458dcfa0fb2880acadcf9f876263a
Adithya S K <adithyaskolavi@gmail.com>
2023-04-01 15:35:33 +0530

    updated web add and solutions - Adithya S K

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -27,8 +27,17 @@ export default function Home() {
               materials and help you ace your exams? Look no further than
               StudyPal - your very own AI study buddy!
             </p>
+            <div>
+              Built by{' '}
+              <Link
+                href={'https://github.com/adithya-s-k'}
+                className="font-semibold text-blue-500 "
+                target="_blank"
+              >
+                Adithya S K
+              </Link>
+            </div>
           </div>
-
           <Link
             href="/upload"
             className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mt-4 "
