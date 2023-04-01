4b0ddddeee0739968057bd9ad57f0e8df7d059ec
Adithya S K <adithyaskolavi@gmail.com>
2023-04-01 13:27:40 +0530

    added dockerfile and made some changes to frontend  - Adithya S K

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -49,20 +49,129 @@ export default function Home() {
           </Link>
         </div>
       </section>
-      <section className="h-[100vh] w-[100vw] flex flex-col justify-center items-center bg-">
-        <div>
-          <a
-            href="#"
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-          >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </a>
+      <section className="h-[100vh] w-[100vw] flex flex-col justify-center items-center bg-gradient-to-b from-black ">
+        <h1>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 md:text-5xl font-extrabold">
+            How it works
+          </span>
+        </h1>
+        <div className="p-10 flex flex-wrap justify-center items-center">
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Topic Summaries
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Our AI technology summarizes complex study materials into
+                bite-sized topics, making it easy for you to understand and
+                retain information.
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Chat with the PDF
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Have a question about a particular topic? Chat with the PDF and
+                get instant answers! Powered By GPT-4
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Quiz Yourself
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Test your knowledge with our interactive quizzes and see how
+                well you've retained the material
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Flashcards
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Our flashcards help you memorize key concepts and definitions,
+                making it easier to recall important information when you need
+                it.
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Personalized Study Plans:
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                StudyPal creates personalized study plans based on your learning
+                style and pace, helping you stay on track and achieve your
+                goals.
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Access to Study Materials
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                With StudyPal, you have access to a library of study materials,
+                including textbooks, articles, and videos, all in one convenient
+                location.
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Audio and Video Summaries
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Listen to audio summaries of study materials to reinforce your
+                understanding and improve retention.
+              </p>
+            </a>
+          </div>
+          <div className="m-4">
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Progress Tracking
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Keep track of your progress and see how far you've come with our
+                progress tracking feature and reach your goals
+              </p>
+            </a>
+          </div>
         </div>
       </section>
     </>
